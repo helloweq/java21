99c04bf62d1523d110f00e6118d1938f7bb24af7
yunjun0603 <nyj16907@gmail.com>
2026-03-27 14:45:46 +0900

    Hello2 commit

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,9 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Hello World2");
 		System.out.println("안녕하세요");
+		
 		
 	}
 
