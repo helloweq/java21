7059a9657e4880ef39cfe647d983f2223a4be47a
yunjun0603 <nyj16907@gmail.com>
2026-04-03 13:40:57 +0900

    exam 업로드

--- src/exam10_method3/TestCat.java
+++ src/exam10_method3/TestCat.java
@@ -1,17 +1,14 @@
-package exam10_method2;
+package exam10_method3;
 
 public class TestCat {
 
 	public static void main(String[] args) {
 		// 야옹이 2 암컷
-		Cat c = new Cat("야옹이", 2, "암컷");
+		Cat c = new Cat("야옹이", 200, "암컷");
 		
-		// c.name="망치"; // 접근불가
-		c.setName("망치");
-		c.setAge(3);
-		c.setGender("수컷");
 		
-		// 조회역할
+		c.setAge(300);
+		
 		String name = c.getName();
 		int age = c.getAge();
 		String gender = c.getGender();
