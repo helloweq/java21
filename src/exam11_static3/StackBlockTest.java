1c575a02588b5a8b29c7ab98f39d0ac7c7a6795a
yunjun0603 <nyj16907@gmail.com>
2026-04-06 10:45:57 +0900

    exam 추가

---
+++ src/exam11_static3/StackBlockTest.java
@@ -0,0 +1,17 @@
+package exam11_static3;
+
+public class StackBlockTest {
+	
+	// static 블럭
+	static {
+		// 순차문, 제어문 사용 가능
+		int num = 10;
+		System.out.println(num);
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
